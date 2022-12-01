ee401d6c98beeaf3f81d0cc93ecbea6b02003760
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-01 18:58:11 +0100

    faster day 1

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -2,20 +2,11 @@ from pathlib import Path
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
-    elves = [0]
-    current_elf = 0
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        food = line.strip()
+    elves_food = [
+        [int(food) for food in elf.split("\n")]
+        for elf in f.read().split("\n\n")
+    ]
+    food_sum = sorted([sum(food) for food in elves_food], reverse=True)
 
-        if food:
-            elves[current_elf] += int(food)
-        else:
-            current_elf += 1
-            elves.append(0)
-
-    elves.sort(reverse=True)
-    print("Top:", elves[0])
-    print("Top 3:", sum(elves[:3]))
+    print(food_sum[0])
+    print(sum(food_sum[:3]))
