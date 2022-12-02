df590db7bab2213761d4f842648446e3f92c0bc9
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-02 18:52:47 +0100

    definitely better

--- src/day2/a.py
+++ src/day2/a.py
@@ -2,29 +2,21 @@ from pathlib import Path
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
+    score = 0
     lose = 0
     draw = 3
     win = 6
 
-    rounds = [
-        [
-            ord(letter) - ord("A") + 1
-            if ord(letter) <= ord("C")
-            else ord(letter) - ord("X") + 1
-            for letter in line.split(" ")
-        ]
-        for line in f.read().split("\n")
-    ]
-
-    #               X       Y       Z
-    #   A           Draw    Win     Lose
-    #   B           Lose    Draw    Win
-    #   C           Win     Lose    Draw
+    #       X       Y       Z
+    #   A   Draw    Win     Lose
+    #   B   Lose    Draw    Win
+    #   C   Win     Lose    Draw
     hands = [[draw, win, lose], [lose, draw, win], [win, lose, draw]]
 
-    score = sum(
-        [hands[opponent - 1][player - 1] + player for (opponent, player) in rounds]
-    )
+    for line in f.read().split("\n"):
+        opponent = ord(line[0:1]) - ord("A")
+        player = ord(line[2:3]) - ord("X")
+        score += hands[opponent][player] + (player + 1)
 
     print(score)
     assert score == 10595
