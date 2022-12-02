153056f6d9b9436eefcda6b14281fb231041f239
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-02 21:05:25 +0100

    Heaven

--- src/day2/a.py
+++ src/day2/a.py
@@ -2,21 +2,16 @@ from pathlib import Path
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
-    score = 0
-
-    #       X       Y       Z
-    #   A   Draw    Win     Lose
-    #   B   Lose    Draw    Win
-    #   C   Win     Lose    Draw
-    hands = [[3, 6, 0], [0, 3, 6], [6, 0, 3]]
+    score_A = 0
+    score_B = 0
 
     for line in f.read().split("\n"):
         opponent = ord(line[0:1]) - ord("A")
         player = ord(line[2:3]) - ord("X")
-        score += hands[opponent][player] + (player + 1)
+        score_A += 1 + player + 3 * ((2 * opponent + player + 1) % 3)
+        score_B += 1 + player * 3 + (3 + opponent + player - 1) % 3
 
-    print(score)
-    assert score == 10595
-
-
-# 10595
+    print(score_A)
+    print(score_B)
+    assert score_A == 10595
+    assert score_B == 9541
