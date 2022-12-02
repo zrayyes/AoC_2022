0cd1cc4b30352e997c58cd1e40c350ff6ee1d4ce
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-02 18:48:50 +0100

    is this better?

--- src/day2/a.py
+++ src/day2/a.py
@@ -2,55 +2,32 @@ from pathlib import Path
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
-    score = 0
+    lose = 0
     draw = 3
     win = 6
-    rounds = [[letter for letter in line.split(" ")] for line in f.read().split("\n")]
 
-    for (opponent, player) in rounds:
-        # Rock
-        if opponent == "A":
-            # Rock
-            if player == "X":
-                score += draw
-            # Paper
-            elif player == "Y":
-                score += win
-            # Scissor
-            elif player == "Z":
-                pass
-        # Paper
-        elif opponent == "B":
-            # Rock
-            if player == "X":
-                pass
-            # Paper
-            elif player == "Y":
-                score += draw
-            # Scissor
-            elif player == "Z":
-                score += win
-        # Scissor
-        elif opponent == "C":
-            # Rock
-            if player == "X":
-                score += win
-            # Paper
-            elif player == "Y":
-                pass
-            # Scissor
-            elif player == "Z":
-                score += draw
+    rounds = [
+        [
+            ord(letter) - ord("A") + 1
+            if ord(letter) <= ord("C")
+            else ord(letter) - ord("X") + 1
+            for letter in line.split(" ")
+        ]
+        for line in f.read().split("\n")
+    ]
 
-        # Extra
-        if player == "X":
-            score += 1
-        if player == "Y":
-            score += 2
-        if player == "Z":
-            score += 3
+    #               X       Y       Z
+    #   A           Draw    Win     Lose
+    #   B           Lose    Draw    Win
+    #   C           Win     Lose    Draw
+    hands = [[draw, win, lose], [lose, draw, win], [win, lose, draw]]
+
+    score = sum(
+        [hands[opponent - 1][player - 1] + player for (opponent, player) in rounds]
+    )
 
     print(score)
+    assert score == 10595
 
 
 # 10595
