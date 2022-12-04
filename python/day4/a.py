f1fb45864050dadf7ea591ffa7a82aa794f6e851
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-04 08:50:30 +0100

    part 2 done

--- python/day4/a.py
+++ python/day4/a.py
@@ -7,9 +7,19 @@ with p.open("r") as f:
         for line in f.read().split("\n")
     ]
 
-    counter = 0
+    counter_A = 0
+    counter_B = 0
     for a, b in pairs:
         if ((a[0] <= b[0]) and (a[1] >= b[1])) or ((b[0] <= a[0]) and (b[1] >= a[1])):
-            counter += 1
+            counter_A += 1
+            counter_B += 1
 
-    print(counter)
+        elif (b[0] <= a[0] <= b[1]) or (b[0] <= a[1] <= b[1]):
+            counter_B += 1
+
+    print(counter_A)
+    print(counter_B)
+
+
+# 657
+# 938
