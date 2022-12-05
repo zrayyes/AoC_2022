93d0c899fd6b9e8dcbaf75d5a7f67b78ae0b4941
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-05 17:49:30 +0100

    moved to one file

--- python/day5/ab.py
+++ python/day5/ab.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from pathlib import Path
+import copy
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
@@ -17,9 +18,23 @@ with p.open("r") as f:
                 column_index = crate_index // 4
                 crates[column_index].append(line[crate_index])
 
+    crates_A = copy.deepcopy(crates)
+    crates_B = copy.deepcopy(crates)
+
     for move in moves:
         move_count, move_from, move_to = move
+        move_to -= 1
+        move_from -= 1
+        temp = []
         for i in range(move_count):
-            crates[move_to - 1] = [crates[move_from - 1].pop(0)] + crates[move_to - 1]
+            crates_A[move_to] = [crates_A[move_from].pop(0)] + crates_A[move_to]
+            temp.append(crates_B[move_from].pop(0))
+        for i in range(len(temp)):
+            crates_B[move_to] = [temp.pop()] + crates_B[move_to]
 
-    print("".join([crates[k].pop(0) for k in range(len(crates))]))
+    print("".join([crates_A[k].pop(0) for k in range(len(crates_A))]))
+    print("".join([crates_B[k].pop(0) for k in range(len(crates_B))]))
+
+
+# BZLVHBWQF
+# TDGJQTZSL
