0e1834a796bfb4cc0d89672e15ab7bc820d9a3f6
Zuhair Rayyes <zuhair@rocketloop.de>
2022-12-06 17:56:15 +0100

    part 2 done

--- python/day6/a.py
+++ python/day6/a.py
@@ -1,15 +1,20 @@
 from pathlib import Path
+import sys
+
+
+marker_length = int(sys.argv[1]) if len(sys.argv) > 1 else 4
 
 p = Path(__file__).with_name("input.txt")
 with p.open("r") as f:
     line = f.read()
     for i, c in enumerate(line):
-        if i >= len(line) - 3:
+        if i >= len(line) - (marker_length - 1):
             break
         else:
-            window = [line[i],line[i+1],line[i+2],line[i+3]]
+            window = line[i : i + marker_length]
             if len(window) == len(set(window)):
-                print(i+4)
+                print(i + marker_length)
                 exit(0)
 
 # 1034
+# 2472
