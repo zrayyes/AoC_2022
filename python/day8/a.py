c427a16c0d61f73864f45c9bdf9527ddfd467469
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-08 13:56:34 +0100

    added get_trees function

--- python/day8/a.py
+++ python/day8/a.py
@@ -14,6 +14,18 @@ class Grid:
         self.height = len(input)
         self.width = len(input[0])
 
+    def get_left_trees(self, y: int, x: int) -> List[int]:
+        return list(reversed(self.grid[y][:x]))
+
+    def get_right_trees(self, y: int, x: int) -> List[int]:
+        return self.grid[y][x + 1 :]
+
+    def get_top_trees(self, y: int, x: int) -> List[int]:
+        return list(reversed([self.grid[v][x] for v in range(y)]))
+
+    def get_bottom_trees(self, y: int, x: int) -> List[int]:
+        return [self.grid[v][x] for v in range(y + 1, self.height)]
+
     def walk(self) -> List[bool]:
         out = []
         for y in range(self.height):
@@ -24,18 +36,16 @@ class Grid:
                     out.append(True)
                 else:
                     # Left
-                    if tree > max(self.grid[y][:x]):
+                    if tree > max(self.get_left_trees(y, x)):
                         out.append(True)
                     # Right
-                    elif tree > max(self.grid[y][x + 1 :]):
+                    elif tree > max(self.get_right_trees(y, x)):
                         out.append(True)
                     # Top
-                    elif tree > max([self.grid[v][x] for v in range(y)]):
+                    elif tree > max(self.get_top_trees(y, x)):
                         out.append(True)
                     # Bottom
-                    elif tree > max(
-                        [self.grid[v][x] for v in range(y + 1, self.height)]
-                    ):
+                    elif tree > max(self.get_bottom_trees(y, x)):
                         out.append(True)
 
         return out
@@ -52,30 +62,28 @@ class Grid:
                     counter = 1
                     # Left
                     left_counter = 0
-                    for left_tree in reversed(self.grid[y][:x]):
+                    for left_tree in self.get_left_trees(y, x):
                         left_counter += 1
                         if tree <= left_tree:
                             break
                     counter *= left_counter
                     # Right
                     right_counter = 0
-                    for right_tree in self.grid[y][x + 1 :]:
+                    for right_tree in self.get_right_trees(y, x):
                         right_counter += 1
                         if tree <= right_tree:
                             break
                     counter *= right_counter
                     # Top
                     top_counter = 0
-                    for top_tree in reversed([self.grid[v][x] for v in range(y)]):
+                    for top_tree in self.get_top_trees(y, x):
                         top_counter += 1
                         if tree <= top_tree:
                             break
                     counter *= top_counter
                     # Bottom
                     bot_counter = 0
-                    for bot_tree in [
-                        self.grid[v][x] for v in range(y + 1, self.height)
-                    ]:
+                    for bot_tree in self.get_bottom_trees(y, x):
                         bot_counter += 1
                         if tree <= bot_tree:
                             break
