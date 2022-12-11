b388ad5ad42928da554c7a71db92eda1557a926c
Zuhair Rayyes <rayyes95@gmail.com>
2022-12-11 11:19:16 +0100

    fixed

--- python/day11/a.py
+++ python/day11/a.py
@@ -6,40 +6,34 @@ from typing import List
 
 @dataclass
 class Monkey:
-    number: int
     items: List[int]
     operation: str
-    test: str
-    test_true: str
-    test_false: str
+    test: int
+    test_true: int
+    test_false: int
     items_inspected = 0
 
 
 class Tree:
-    monkeys: List[Monkey] = []
-    round_number = 0
+    monkeys: List[Monkey]
+    round_number: int
 
     def __init__(self) -> None:
-        pass
+        self.monkeys = []
+        self.round_number = 0
 
     def __str__(self) -> str:
         out = ""
-        for monkey in self.monkeys:
-            out += f"Monkey {monkey.number}: {monkey.items}. inspected items {monkey.items_inspected} times.\n"
+        for i, monkey in enumerate(self.monkeys):
+            out += f"Monkey {i}: {monkey.items}. Inspected items {monkey.items_inspected} times.\n"
         out += f"Rounds: {self.round_number}\n"
         return out
 
     def add_monkey(self, monkey: Monkey):
         self.monkeys.append(monkey)
 
-    def get_monkey_index_by_number(self, monkey_number: int) -> int:
-        return [
-            i for i, monkey in enumerate(self.monkeys) if monkey.number == monkey_number
-        ][0]
-
     def throw_to_monkey(self, monkey_number: int, item: int):
-        monkey_index = self.get_monkey_index_by_number(monkey_number)
-        self.monkeys[monkey_index].items.append(item)
+        self.monkeys[monkey_number].items.append(item)
 
     def get_monkey_business(self) -> int:
         return math.prod(
@@ -48,29 +42,28 @@ class Tree:
             ]
         )
 
-    def start_round(self):
+    def start_round(self, no_worries=True):
         for monkey in self.monkeys:
             while monkey.items:
                 # Inspect
                 old = monkey.items.pop(0)
                 new_item = eval(monkey.operation)
-                new_item = new_item // 3
-                monkey.items_inspected += 1
+
+                if no_worries:
+                    new_item = new_item // 3
 
                 # Test
-                test = monkey.test[13:]
-                # Divisible by ...
-                if new_item % int(test) == 0:
-                    new_monkey = int(monkey.test_true[16:])
-                    self.throw_to_monkey(new_monkey, new_item)
+                if new_item % monkey.test == 0:
+                    self.throw_to_monkey(monkey.test_true, new_item)
                 else:
-                    new_monkey = int(monkey.test_false[16:])
-                    self.throw_to_monkey(new_monkey, new_item)
+                    self.throw_to_monkey(monkey.test_false, new_item)
 
+                monkey.items_inspected += 1
         self.round_number += 1
 
 
-tree = Tree()
+tree_1 = Tree()
+tree_2 = Tree()
 
 p = Path(__file__).with_name("input.txt")
 with p.open() as f:
@@ -83,18 +76,24 @@ with p.open() as f:
         test_false = ""
 
         lines = monkey.split("\n")
-        number = int(lines[0][7])
         starting_items = list(map(lambda x: int(x), lines[1][18:].split(", ")))
         operation = lines[2][19:]
-        test = lines[3][8:]
-        test_true = lines[4][13:]
-        test_false = lines[5][14:]
+        test = int(lines[3][21:])
+        test_true = int(lines[4][29:])
+        test_false = int(lines[5][30:])
 
-        monkey = Monkey(number, starting_items, operation, test, test_true, test_false)
-        tree.add_monkey(monkey)
+        monkey = Monkey(starting_items, operation, test, test_true, test_false)
+        tree_1.add_monkey(monkey)
+        tree_2.add_monkey(monkey)
 
-print(tree)
+# Part 1
 for i in range(20):
-    tree.start_round()
-print(tree)
-print(tree.get_monkey_business())
+    tree_1.start_round()
+print(tree_1)
+print(tree_1.get_monkey_business())
+
+# Part 2
+# for i in range(10000):
+#     tree_2.start_round(no_worries=False)
+# print(tree_2)
+# print(tree_2.get_monkey_business())
